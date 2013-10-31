f1bea522ac8815dc7a11fc708cd8353d0f2465f8
gavinlvincent <gavin.l.vincent@gmail.com>
2013-10-31 14:54:41 -0600

    js upgrades

--- support/stores/locator/soCal/js/page.js
+++ support/stores/locator/soCal/js/page.js
@@ -23,7 +23,37 @@ if (document.documentElement.clientWidth <= 1099) {
 		});
 		$(this).toggleClass('rotate_chevron');
 	});
+} else {
+	$('#stores_map_details').show();
 }
+
+	$(window).resize(function(){
+		if ($(window).width() <= 1099) {
+			// Legend SlideUp
+			$('.locate-stores-filters legend').click(function() {
+				$('.filter-section').slideToggle('slow', function() {
+					// Animation complete.
+				});
+				$(this).toggleClass('rotate_chevron');
+			});			
+		} else if($(window).width() > 1099){
+			$('#stores_map_map').show();
+			$('#stores_map_details').show();
+		}
+		
+		currState = $('.tabs ul li').closest('.active').text();
+		
+		if ($(window).width() >= 500 && $(window).width() <= 1099){
+			$('#stores_map_details').hide();
+			$('#stores_map_map').hide();
+			
+			if(currState == 'Map'){
+				$('#stores_map_map').show();
+			} else {
+				$('#stores_map_details').show();
+			}
+		}
+	});
 });
 
 
